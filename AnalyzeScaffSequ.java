26bf73a2f13e3eede281673aae9e8e5fbfc8de19
Christian Wachauf <christianwachauf@Palmer-Eldritch.dhcp.e14.ph.tum.de>
2017-01-11 18:51:17 +0100

    modified file

--- AnalyzeScaffSequ.java
+++ AnalyzeScaffSequ.java
@@ -5,7 +5,7 @@ public class AnalyzeScaffSequ
 	private static char[] bases = {'A','C','G','T'};
 	private int letter_frequencies[];
 	private String sequ;
-	
+		
 	public AnalyzeScaffSequ(String sequ)
 	{
 		this.sequ=sequ;
@@ -31,6 +31,30 @@ public class AnalyzeScaffSequ
 		}
 	}
 	
+	public boolean CheckDBProperty(int order)
+	{
+		ArrayList<String> list_of_tupels = new ArrayList<String>();
+		Set<String> set_of_tupels = new HashSet<String>();
+		int length = this.sequ.length();
+		for(int i=0;i<length-order+1;i++)
+		{
+			list_of_tupels.add(this.sequ.substring(i,i+order));
+		}
+		
+		// loop over elements in list, check for duplicates:
+		for(int i=0;i<list_of_tupels.size();i++)
+		{
+				if(!set_of_tupels.add(list_of_tupels.get(i)))
+				{
+					System.out.println("the following tuple already exists: ");
+					System.out.println(list_of_tupels.get(i));
+					return false;
+				}
+		}
+		System.out.println(list_of_tupels);
+		return true;
+	}
+	
 	public void PrintLetterFrequencies()
 	{
 		for(int i=0;i<4;i++)
@@ -46,5 +70,6 @@ public class AnalyzeScaffSequ
 		String scaff_sequ = new String("ACTCTCCAGCAATCGCCCTAAATCAGTC");
 		AnalyzeScaffSequ anscas = new AnalyzeScaffSequ(scaff_sequ);
 		anscas.PrintLetterFrequencies();
+		anscas.CheckDBProperty(5);
 	}
 }
